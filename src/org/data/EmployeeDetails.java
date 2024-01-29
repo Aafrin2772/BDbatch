b1f661eb994207baec6f7637614868a556b116cd
Aafrin2772 <nasee2772@gmail.com>
2024-01-29 14:51:05 +0530

    Employee details class added successfully

---
+++ src/org/data/EmployeeDetails.java
@@ -0,0 +1,12 @@
+package org.data;
+
+public class EmployeeDetails {
+	
+	private void empName() {
+       System.out.println("Emp name:afrin");
+	}
+	
+    private void empAge() {
+		System.out.println("Emp age :42");
+	}
+}
