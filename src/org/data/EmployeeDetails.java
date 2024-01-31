084160bb28a593716a2bca82edd5a74532d5733e
Aafrin2772 <nasee2772@gmail.com>
2024-01-31 13:54:39 +0530

    Conflict resolved

--- src/org/data/EmployeeDetails.java
+++ src/org/data/EmployeeDetails.java
@@ -10,13 +10,13 @@ public class EmployeeDetails {
 		System.out.println("Emp age :42");
 	}
     
-<<<<<<< HEAD
-    private void empId() {
-		System.out.println("Emp id:12345");
-=======
     private void empLoc() {
 		System.out.println("Emp loc: trichy");
->>>>>>> 0f090a080d100c4dcf3aa479d486ba30bd9f0031
+
 	}
+    private void empId() {
+		System.out.println("Emp id:12345");
+
+    }
     
 }
